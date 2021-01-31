d513f1aed3cf6022fb065a6b3a27e0b1cf917943
Ali El Husseini <elhusseiniali@icloud.com>
2021-01-31 23:38:52 +0100

    Add string_of_tm to represent lambda terms as strings.

--- main.ml
+++ main.ml
@@ -19,7 +19,7 @@ type ty =
 type tm =
     | Var of var
     | App of (var * var)
-    | Abs of (var * ty * var)
+    | Abs of (var * ty * tm)
 
 
 let rec string_of_ty e =
@@ -28,17 +28,43 @@ let rec string_of_ty e =
     | Impl(t1, t2) -> String.concat "" ["("; String.concat " \u{21d2} " [string_of_ty(t1); string_of_ty(t2)]; ")"]
 
 
-let x1:tvar = "x1";;
-let x2:tvar = "x2";;
+
+let rec string_of_tm e =
+  match e with
+  | Var(t) -> t
+  | App(a, b) -> String.concat "" ["("; String.concat " " [a; b]; ")"]
+  | Abs(a, t, b) -> String.concat "" ["\u{03bb}"; "("; a; ": "; string_of_ty(t); ")."; string_of_tm(b)]
 
 
-let xt1:ty = Term(x1);;
-let xt2:ty = Term(x2);;
-let xt:ty = Impl(xt1, xt2);;
-let xt3:ty = Impl(xt1, xt);;
+
+let a:tvar = "A";;
+let b:tvar = "B";;
+let c:tvar = "C";;
+
+
+let at:ty = Term(a);;
+let bt:ty = Term(b);;
+let ct:ty = Term(c);;
+
+let abt:ty = Impl(at, bt);;
+let act:ty = Impl(at, ct);;
+let abact:ty = Impl(abt, act);;
+
+
+let x:var = "x";;
+let f:var = "f";;
+
+
+let xvar:tm = Var(x);;
+let fx:tm = App(f, x);;
+let xf:tm = Abs(x, at, fx);;
+
+let lt1:tm = Abs(f, abt, fx);;
+let lt2:tm = Abs(f, abt, xf);;
 
 
 let () =
-    print_endline (string_of_ty(xt));;
-    print_endline (string_of_ty(xt3))
-
+    print_endline (string_of_ty(abact));;
+    print_endline (string_of_tm(fx));;
+    print_endline (string_of_tm(lt1));;
+    print_endline (string_of_tm(lt2));;
